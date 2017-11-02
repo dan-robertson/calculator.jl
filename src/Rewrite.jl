07815bb28dcaae6d39bd07270b3da315f471e523
Dan Robertson <dan.robertson14@gmail.com>
2017-11-02 18:40:57 +0000

    Began implementation of rewriting engine
    Reduce.jl seems to be more trouble than its worth. Here is the begging
    of an engine for processing rewriting rules for julia expressions.
    
    There are a few worts with julia expressions which we try to ignore.
    One is that julia has / and //. We use / for all purposes and hope we
    don't see a //.
    
    The rewriting engine is mainly about matching up variables in a match
    expression (i.e. the first half of a rule) against arbitrary
    expressions. Most functions are not treated specially but we give
    special treatment to expressions invloving +, *, -, etc in the match
    expression. In particular, we try to convert the matcher and the thing
    we are matching against into a word in the corresponding group (that
    is, a sequence of expressions and powers) and then match in a sensible
    way based on that.
    
    This method isn't perfect. There's no way to specify that some symbol
    may take the identity element (so we assume that it cannot) and we
    currently don't consider splitting up powers as part of the matching,
    for example to match a+b against 5+5, 5+5 gets converted into the word
    2*5 and then we can't split this into 1*5+1*5 to get the right match.
    
    Also a+a can succesfully match against 5+5 and 2*5 as 2*5 gets
    converted into the word (1*(2*5)) in the group of +, and then a*2 is
    recursively matched against 2*5 (i.e. in the group of *) which is
    successful. Whereas a*2 cannot match against 5+5.
    
    The main entry point is Rewrite.rewrite.
    
    The rules themselves have not yet been written.

---
+++ src/Rewrite.jl
@@ -0,0 +1,540 @@
+module Rewrite
+
+using FunctionalCollections
+
+### This does algebra (in the 18th century sense) by rewrite rules.
+
+### Commentary:
+
+## Rewriting is split into two steps: matching and substituting
+## Suppose we have a rule a => b, and we want to rewrite an expression e
+## Then matching is the processes of finding a subexpression e' that looks like a inside e,
+## and determining what variable substitutions need to happen inside a to unify a with e'.
+## substituting is the process of getting from the match to the rewritten expression
+##
+## Note that in expressions we use / for division, never //
+##
+## The method for matching is
+## match(expression, toMatch, mapping)
+## with mapping being a PersistentHashMap{Symbol, Any}
+## This should return a list of mappings, each being mapping
+## extended by the result of matching the expression if there
+## is a match, and throw an error if there are not mappings.
+## This does not search subexpressions so match(:(x + (y * z)), :(a + b), mapping) should error.
+## Matching generally uses the unify method
+## 
+## The method for substituting is substitute(substituter, mapping)
+## It is somewhat simpler and is happy for substituter to be either a function,
+## in which case it is called with the mapping as KW arguments
+## or for it to be an Expr in which case any matching symbols are substituted
+
+const Mapping = PersistentHashMap{TypeVar(:S,Symbol,Any), TypeVar(:T,Any)}
+const emptyMapping = phmap()
+immutable ReallyNotFound end
+
+immutable UnificationError <: Exception
+    symbol :: Symbol
+    val1; val2 # the two things we failed to unify
+end
+immutable MatchingError <: Exception
+    expression; matcher
+end
+
+mapping(x::Symbol,v) = unify(emptyMapping,@Persistent Dict(x => v))
+
+function unify(m1::Mapping,m2::Mapping)
+    default = ReallyNotFound()
+    result = m1
+    for (k,v2) in m2
+        v1 = get(m1,k,default)
+        if v1 == default
+            result = assoc(m1,k,v2)
+        elseif v1 == v2 # TODO: a more general equality test
+            # result = result
+        else
+            throw(UnificationError(k, v1, v2))
+        end
+    end
+    result
+end
+
+unify(m1::Mapping) = m1
+unify(m1, m2) = unify(unify(m1),unify(m2))
+unify(m1::Mapping,m2,m3,ms...) = unify(unify(m1,m2),m3,ms...)
+
+unify{T}(m :: Pair{Symbol,T}) = mapping(m[1],m[2])
+unify{T}(m :: Tuple{Symbol,T}) = mapping(m[1],m[2])
+
+function match(expression::Expr, matcher, mapping)
+    matchExpr(Val{expression.head}(),expression, matcher, mapping)
+end
+
+match(x::Symbol, m::Symbol, mapping) = [unify(mapping, m => x)]
+function match(x::Symbol, m::Expr, mapping)
+    if m.head == :call
+        alg = getAlgebra(m.args[1])
+        if alg != NoAlgebra()
+            return matchAlgebra(alg, x, m, mapping)
+        end
+    end
+    throw(MatchingError(x,m))
+end
+
+# matching some kind of object (not Expr/symbol).
+match(object::Expr, matcher::Symbol, mapping) = [unify(mapping, matcher=>object)]
+match(object, matcher::Symbol, mapping) = [unify(mapping, matcher=>object)]
+function match(object, matcher, mapping)
+    if object == matcher
+        [mapping]
+    else
+        expr = expressionify(object)
+        if expr == object
+            throw(MatchingError(object,matcher))
+        else
+            match(expr, matcher, mapping)
+        end
+    end
+end
+
+# A way of converting objects like 1//2 into expressions like :(1 / 2)
+expressionify(x) = x
+expressionify(x::Rational) = :( $(x.num) / $(x.den) )
+expressionify(x::Complex) = :( $(real(x)) + ($(imag(x)) * im))
+
+function matchMany(exprs, matchers, mapping)
+    # TODO: try to pick a good order to do the matching
+    mappings = Set([mapping])
+    for (e,m) in zip(exprs,matchers)
+        mappings2 = Set()
+        for mapping in mappings
+            try
+                for mapping2 in match(e,m,mapping)
+                    push!(mappings2, mapping2)
+                end
+            end
+        end
+        mappings = mappings2
+    end
+    res = collect(mappings)
+    if length(res) == 0
+        throw(MatchingError(exprs,matchers))
+    end
+    res
+end
+
+
+function matchExpr(::Val{:macrocall}, expression, matcher, mapping)
+    m = args[1]
+    if m == :(@int128_str) || m == :(@big_str)
+        int = tryparse(BigInt, expression.args[2])
+        if isnull(int)
+            match(parse(BigFloat, expression.args[2]), matcher, mapping)
+        else
+            match(get(int), matcher, mapping)
+        end
+    else
+        throw("Don't know how to handle macrocall $(args[1])")
+    end
+end
+
+# for a general call we dispatch based on what is being called
+# e.g. sin(x) is not treated specially but x * y === *(x,y) is.
+function matchExpr(::Val{:call}, expression, matcher::Expr, mapping)
+    if matcher.head != :call
+        throw(MatchingError(expression, matcher))
+    end
+    alg = if isa(matcher.args[1], Expr)
+        if matcher.args[1].head == :$
+            NoAlgebra()
+        else
+            throw(MatchingError(expression, matcher))
+        end
+    else
+        getAlgebra(Val{matcher.args[1]}())
+    end
+    matchAlgebra(alg, expression, matcher, mapping)
+end
+
+abstract Algebra
+immutable NoAlgebra <: Algebra end
+immutable GroupAlgebra <: Algebra
+    identity               # e.g. 0   1    Arity 0
+    op :: Symbol           # e.g. +   *    Arity Any
+    inverse :: Symbol      # e.g. -   inv  Arity 1
+    opinv :: Symbol        # e.g. -   /    Arity 2
+    pow   :: Symbol        # e.g. *   ^    Arity 2 (1 in group, 1 in Z)
+    matchPow :: Bool
+    commutative :: Bool
+    canonicaliseType
+end
+abstract Canonicalisation
+immutable AdditionType <: Canonicalisation end
+immutable MultiplicationType <: Canonicalisation end
+
+getAlgebra(other) = NoAlgebra()
+getAlgebra(::Val{:+}) = GroupAlgebra(0,:+,:-,:-,:*,false,true,AdditionType())
+getAlgebra(::Val{:-}) = getAlgebra(Val{:+})
+getAlgebra(::Val{:*}) = GroupAlgebra(0,:*,:inv,:/,:^,true,true,MultiplicationType())
+getAlgebra(::Val{:/}) = getAlgebra(Val{:*})
+getAlgebra(::Val{:inv}) = getAlgebra(Val{:*})
+
+
+"""
+When matching a function call we have either
+match(foo(args...), foo(matchers...)) -> may succeed
+match(foo(args...), \$f(matcher...)) -> may succeed with f => foo
+"""
+function matchAlgebra(::NoAlgebra, expr::Expr, matcher::Expr, mapping)
+    fname = expr.args[1]
+    mfun  = matcher.args[1]
+    # match function name
+    fmapping = mapping
+    if isa(mfun, Symbol)
+        if fname == mfun
+            # nothing to do
+        else
+            throw(MatchingError(expr, matcher))
+        end
+    elseif isa(mfun, Expr) && mfun.head == :$
+        fmapping = unify(fmapping, mfun.args[1] => fname)
+    end
+    if length(expr.args) != length(matcher.args)
+        throw(MatchingError(expr, matcher))
+    elseif length(expr.args) == 1
+        [fmapping]
+    else
+        matchMany(expr.args[2:end], matcher.args[2:end], fmapping)
+    end
+end
+
+# convert an expression into a word in a group g
+# a word is a list representing a product of elements in g
+# it is a list of the form [(elt,pow)...]
+function toWord(g::GroupAlgebra, expr)
+    if expr == g.identity
+        [(g.identity,0)]
+    else
+        [(expr,1)]
+    end
+end
+
+function toWord(g::GroupAlgebra, expr::Expr)
+    if expr.head != :call
+        (expr,1)
+    else
+        fun = expr.args[1]
+        if fun == g.op
+            multiplyWords(g, map(x -> toWord(g,x), expr.args[2:end])...)
+        elseif fun == g.inverse && length(expr.args) == 2
+            invWord(g, toWord(g,expr.args[2]))
+        elseif fun == g.opinv && length(expr.args) == 3
+            multiplyWords(g, toWord(g, expr.args[2]), invWord(g, toWord(g, expr.args[3])))
+        elseif g.matchPow && fun == g.pow && length(expr.args) == 3 &&
+               isa(expr.args[3],Integer) && g.commutative
+            w = toWord(g, expr.args[2])
+            pow = expr.args[3]
+            [(x,p*pow) for (x,p) in w]
+        else
+            [(expr,1)]
+        end
+    end
+end
+
+function multiplyWords(g, words...)
+    r = ()
+    if g.commutative
+        d = Dict()
+        for wordy in words
+            word = isa(wordy, NTuple{2}) ? [wordy] : wordy
+            for (exp,pow) in word
+                newPow = pow + get(d,exp,0)
+                if newPow != 0
+                    push!(d,exp=>newPow)
+                else
+                    delete!(d,exp)
+                end
+            end
+        end
+        r = d
+    else
+        w = []
+        for wordy in words
+            word = isa(wordy, NTuple{2}) ? [wordy] : wordy
+            for (x,pow) in words
+                if pow != 0 && x != g.identity
+                    if length(w) > 0 && w[end][1] == x
+                        newPow = w[end][2] + pow
+                        if newPow == 0
+                            pop!(w)
+                        else
+                            w[end] = (w[end][1],newPow)
+                        end
+                    else
+                        push!(w,(x,pow))
+                    end
+                end
+            end
+        end
+        r = w
+    end
+    if length(r) == 0
+        [(g.identity,1)]
+    else
+        collect((a,b) for (a,b) in r)
+    end
+end
+
+invWord(g,word::NTuple{2}) = [(word[1],-word[2])]
+function invWord(g,word)
+    if g.commutative
+        collect((x,-p) for (x,p) in word)
+    else
+        collect((x,-p) for (x,p) in reverse(word))
+    end
+end
+
+fromWord(g::GroupAlgebra,word) = fromWord(g,g.canonicaliseType,word)
+function fromWord(g::GroupAlgebra,word::NTuple{2})
+    if word[2] == 1
+        word[1]
+    elseif word[2] == -1
+        Expr(:call,g.inverse,word[1])
+    else
+        Expr(:call,g.pow,word[1],word[2])
+    end
+end
+function fromWord(g::GroupAlgebra,::AdditionType,word)
+    if length(word) == 0
+        g.identity
+    else
+        exp = fromWord(g,word[1])
+        lastPlus = false
+        for (x,pow) in drop(word,1)
+            if pow > 0 && lastPlus
+                push!(exp.args, pow == 1 ? x : Expr(:call, g.pow, x, pow))
+            elseif pow > 0
+                exp = Expr(:call, g.op, exp, pow == 1 ? x : Expr(:call, g.pow, x, pow))
+                lastPlus = true
+            elseif pow < 0
+                exp = Expr(:call, g.opinv, exp, pow == -1 ? x : Expr(:call, g.pow, x, -pow))
+            end
+        end
+        exp
+    end
+end
+function fromWord(g::GroupAlgebra,::MultiplicationType,word)
+    top = collect((x,pow) for (x,pow) in word if pow > 0)
+    bot = collect((x,-pow) for (x,pow) in word if pow < 0)
+    xtop = ()
+    if length(top) == 0 
+        xtop = g.identity
+    else
+        args = collect(pow == 1 ? x : Expr(:call,g.pow,x,pow) for (x,pow) in top)
+        xtop = length(args) == 1 ? args[1] : Expr(:call,g.op,args...)
+    end
+    xbot = ()
+    if length(bot) == 0 
+        xtop
+    else
+        args = collect(pow == 1 ? x : Expr(:call,g.pow,x,pow) for (x,pow) in bot)
+        xbot = length(args) == 1 ? args[1] : Expr(:call,g.op,args...)
+        Expr(:call,g.opinv,xtop,xbot)
+    end
+end
+
+function matchAlgebra(g::GroupAlgebra, object, matcher::Expr, mapping)
+    if !g.commutative
+        throw(MatchingError(object,matcher))
+    end
+    # object is not something 
+    matchWord = toWord(g, matcher)
+    # for the sake of sanity we only permit matching against symbols or literals
+    literalBits = collect((x,-p) for (x,p) in matchWord if !isa(x,Symbol))
+    expr = object
+    if length(literalBits) == 0 || length(literalBits) == 1 && 
+        (literalBits[1][2] == 0 || literalBits[1][1] == g.identity)
+        expr = object
+    else
+        expr = Expr(:call,g.op,object,fromWord(g,literalBits))
+    end
+    identmap = unify((x=>g.identity for (x,p) in matchWord if isa(x,Symbol) && abs(p) > 1)...)
+    symbolBits = collect((x,p) for (x,p) in matchWord if isa(x,Symbol) && abs(p) == 1)
+    map = unify(mapping,identmap)
+    mappings = []
+    for (x,p) in symbolBits
+        try
+            res = unify(map,p>0 ? x=>expr : x=>Expr(:call, g.inv, expr),
+                        (y=>g.identity for (y,p) in symbolBits if y != x)...)
+            push!(res, mappings)
+        end
+    end
+    if length(mappings) == 0
+        throw(MatchingError(object,matcher))
+    end
+    mappings
+end
+
+function matchAlgebra(g::GroupAlgebra, expr::Expr, matcher::Expr, mapping)
+    # determine if expr is an expression in g
+    exword = toWord(g,expr)
+    maword = toWord(g,matcher)
+    mappings = Set()
+    if !g.commutative
+        error("TODO: match noncommutative groups")
+    end
+    mag_helper_com(g, mappings, mapping, exword, start(exword),
+                   maword, start(maword))
+    if length(mappings) == 0
+        throw(MatchingError(expr, matcher))
+    else
+        collect(mappings)
+    end
+end
+
+function mag_helper_com(g::GroupAlgebra, mappings::Set, mapping, exword_iter, exword_state,
+                        maword_iter, maword_state)
+    if !done(maword_iter, maword_state)
+        (matcher, pow), state2 = next(maword_iter, maword_state)
+        if pow == 0
+            # skip as doesn't contribute
+            mag_helper_com(g, mappings, mapping, exword_iter, exword_state, maword_iter, state2)
+        elseif done(maword_iter, state2)
+            # special case: one matcher left, so match it against everything left
+            exword = collect(rest(exword_iter, exword_state))
+            if length(exword) == 0
+                return # We don't match against id yet
+                exword = [(g.identity, pow)]
+            end
+            gcdpow = reduce(gcd, (pow for (x,pow) in exword))
+            if mod(gcdpow,pow) == 0 && pow != 1
+                rootword = [(x,div(p,pow)) for (x,p) in exword]
+                try
+                    mappings2 = match(fromWord(g,rootword), matcher, mapping)
+                    for mapping2 in mappings2
+                        push!(mappings, mapping2)
+                    end
+                    return # It worked so don't need to try the other way
+                end
+            end
+            try
+                matcher2 = pow == 1 ? matcher : Expr(:call, g.pow, matcher, pow)
+                mappings2 = match(fromWord(g,exword), matcher2, mapping)
+                for mapping2 in mappings2
+                    push!(mappings, mapping2)
+                end
+            end
+        else
+            mag_helper_com1(g, mappings, mapping, exword_iter, exword_state,
+                            maword_iter, state2, matcher, pow, [], [])
+        end
+    elseif done(exword_iter, exword_state)
+        push!(mappings, mapping)
+    end
+end
+
+function mag_helper_com1(g::GroupAlgebra, mappings::Set, mapping, exword_iter, exword_state,
+                         maword_iter, maword_state, matcher, pow, exword_part, exword_skip)
+    if !done(exword_iter, exword_state)
+        # match against one letter
+        (x, pow2), state2 = next(exword_iter, exword_state)
+        exword = [exword_part ; (x,pow2)]
+        gcdpow = reduce(gcd, (pow for (x,pow) in exword))
+        skipNormalMatch = false
+        if mod(gcdpow,pow) == 0 && pow != 1
+            # we can match m^pow against exword as m against exword^(1/pow)
+            rootword = [(x,div(p,pow)) for (x,p) in exword]
+            try
+                println("match $(fromWord(g,rootword)), $matcher")
+                mappings2 = match(fromWord(g,rootword), matcher, mapping)
+                for mapping2 in mappings2
+                    it, st = if length(exword_skip) > 0
+                        iter = Base.flatten((exword_skip, rest(exword_iter, state2)))
+                        iter, start(iter)
+                    else
+                        exword_iter, state2
+                    end
+                    mag_helper_com(g, mappings, mapping2, it, st,
+                                   maword_iter, maword_state)
+                end
+                # there was a match so we can skip the normal try
+                skipNormalMatch = true
+            end
+        end
+        # we try to match exword against m^pow
+        if !skipNormalMatch
+            try
+                matcher2 = pow == 1 ? matcher : Expr(:call, g.pow, matcher, pow)
+                mappings2 = match(fromWord(g,exword), matcher2, mapping)
+                for mapping2 in mappings2
+                    it, st = if length(exword_skip) > 0
+                        iter = Base.flatten((exword_skip, rest(exword_iter, state2)))
+                        iter, start(iter)
+                    else
+                        exword_iter, state2
+                    end
+                    mag_helper_com(g, mappings, mapping2, it, st,
+                                   maword_iter, maword_state)
+                end
+            end
+        end
+        # we try extending the current word
+        mag_helper_com1(g, mappings, mapping, exword_iter, state2,
+                        maword_iter, maword_state, matcher, pow, exword, exword_skip)
+        # we try skipping the current letter
+        mag_helper_com1(g, mappings, mapping, exword_iter, state2, maword_iter, maword_state,
+                        matcher, pow, exword_part, [exword_skip ; (x,pow)])
+        # TODO: if abs(pow) > 1 try splitting it up. Should only do this if abs(pow) is small though
+    else
+        # Done
+        # Todo: maybe try matching against identity (if length(exword_part) == 0)
+    end
+end
+
+
+
+# Rewriting function
+
+function substitute(substituter::Function, mapping::Mapping)
+    substituter(;mapping...)
+end
+
+function substitute(substituter, mapping::Mapping)
+    substituter
+end
+
+substitute(substituter::Symbol, mapping::Mapping) = get(mapping, substituter, substituter)
+function substitute(substituter::Expr, mapping::Mapping)
+    Expr(substituter.head,
+         (isa(arg,Union{Symbol,Expr}) ? substitute(arg,mapping) : arg
+          for arg in substituter.args)...)
+end
+
+export rewrite
+function rewrite(expr, matcher, sub, limit=20, n=0)
+    if n >= limit
+        return expr
+    end
+    ms = ()
+    try
+        m = match(expr, matcher, emptyMapping)
+        if length(m) > 0
+            ms = m[1]
+        end
+    end
+    if ms != ()
+        expr2 = substitute(sub, ms)
+        rewrite(expr2, matcher, sub, limit, n+1)
+    else
+        # recurse
+        if isa(expr, Expr) && expr.head == :call
+            rewritten = [i == 1 ? arg : rewrite(arg, matcher, sub, limit, n)
+                         for (i,arg) in enumerate(expr.args)]
+            Expr(:call, rewritten...)
+        else
+            expr
+        end
+    end
+end
+
+# End of Module1
+end
